a71418a992417ab3ca887c75e3968cddfd5e8ac1
CarlAxelAven <lexalrac@gmail.com>
2022-11-30 20:45:14 +0100

    abdsk

--- Script1.js
+++ Script1.js
@@ -1,5 +1,9 @@
 // JavaScript source code
 
+//    var datum;
+//    var tid;
+//    let bana; 
+
 function spara() {
 	var datum = document.getElementById("datum").value;
 	var tid = document.getElementById("time").value;
@@ -11,19 +15,21 @@ function spara() {
  		if (radioButton.checked) {
 			bana = radioButton.value;
 			
-	 console.log(tid)
-	 console.log(datum)
-	 console.log(bana)
 			break;
  		}
  	}
 	
- })
-// 	console.log(tid)
-// 	console.log(datum)
-// 	console.log(bana)
+ })	
 
 }
+
+function checking () {
+	console.log(tid)
+	console.log(datum)
+ 	console.log(bana)
+
+ }
+ 
 function kalender() {
 
 	document.getElementById("top").innerHTML = "Kalender grejsilimojs. Du har ans�kt om att spela kl." + tid + ".00 den ";
@@ -39,12 +45,26 @@ function bokbana() {
 function bokomkl() {
 	document.getElementById("omkl").hidden = "";
 	document.getElementById("button2").hidden = "hidden";
+// 	const button2 = document.querySelector('#button2');
+// 	const radioButtons = document.querySelectorAll('input[name="valavbana"]');
+//  button2.addEventListener("click", () => {
+//  	let bana;
+//  	for (const radioButton of radioButtons) {
+//  		if (radioButton.checked) {
+// 			bana = radioButton.value;
+// 			document.getElementById("top").innerHTML = "Omklädningsrum grejsilimojs" + br + "";
+// 			break;
+//  		}
+//  	}
+	
+//  })	
 }
 function bokbastu() {
 	document.getElementById("bastu").hidden = "";
 	document.getElementById("button3").hidden = "hidden";
 }
 function omkl() {
+
 	document.getElementById("top").innerHTML = "Omkl�dningsrum grejsilimojs";
 }
 function bastu() {
